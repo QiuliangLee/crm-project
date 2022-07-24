57db8088e4fbe16c5bf9b6c2da205733d60c89a9
Lucas <779147708@qq.com>
2022-07-24 20:05:50 +0800

    master thrid commit

--- main/java/com/bjpowernode/crm/workbench/web/controller/WorkbenchIndexController.java
+++ main/java/com/bjpowernode/crm/workbench/web/controller/WorkbenchIndexController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WorkbenchIndexController {
     @RequestMapping("/workbench/index.do")
-    public String index(){
+    public String index() {
         //跳转到业务主页面
         return "workbench/index";
     }
